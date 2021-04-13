55543cd76eec66d4ef4dff3b0c6360d6d9b71eae
pettermkuo <petterm0023@gmail.com>
2021-04-13 15:06:53 -0300

    rename users to usersRouter

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
+var usersRouter = require('./routes/usersRouter');
 
 var app = express();
 
--- routes/usersRouter.js
+++ routes/usersRouter.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var router = express.Router();
+const usersController = require('../controllers/usersController')
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
