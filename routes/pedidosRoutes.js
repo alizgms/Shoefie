725adf1bb23d97b25c798ab6ddd6e46e4e1e48bf
Herbert Paes <8264908+hpaes@users.noreply.github.com>
2021-05-05 10:17:51 -0300

    paginação de icons, mudanças de rotas e controllers

--- routes/pedidosRoutes.js
+++ routes/pedidosRoutes.js
@@ -3,10 +3,13 @@ const pedidosController = require('../controllers/pedidosController');
 const router = express.Router();
 
 // middleware
-// const loginAuthenticate = require('../middlewares/LoginAuthenticate');
+const loginAuthenticate = require('../middlewares/LoginAuthenticate');
 
-// router.use(loginAuthenticate);
+router.use(loginAuthenticate);
+// Acompanhar envio
 router.get('/shipping', pedidosController.shippingDetail);
+// Finalizar pagamento
+router.get('/checkout', pedidosController.checkout);
 router.post('/', pedidosController.store);
 
 module.exports = router;
